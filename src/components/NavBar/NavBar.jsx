4bb44b4a44ec554789894a5088a5620c08d7cebe
Camila Pardini <camilabpardini@gmail.com>
2021-08-29 00:01:26 -0300

    Agregado del componente NavBar con sus estilos

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+
+import styles from './NavBar.css'
+import React from 'react'
+
+
+function NavBar() {
+    return (
+        <div className= "nav">
+          <ul className= "menu">
+            <li className= "itemsNav">Botellas</li>
+            <li className= "itemsNav">Latas</li>
+            <li className= "itemsNav">Saborizadas</li>
+          </ul>
+        </div>
+    )
+}
+
+export default NavBar
+
+
+
+
+
